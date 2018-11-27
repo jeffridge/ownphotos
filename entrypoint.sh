a16ccb32249a4c10dd68c31f32735e17bd8f0e88
Jeff Laughlin <jeff.ridge@gmail.com>
2018-11-27 17:17:37 -0500

    fix url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cp /code/nginx.conf /etc/nginx/sites-enabled/default
-BACKEND_URL=${BACKEND_PROTOCOL}${BACKEND_HOST}
+BACKEND_URL=${BACKEND_PROTOCOL}://${BACKEND_HOST}
 sed -i -e 's/replaceme/'"$BACKEND_HOST"'/g' /etc/nginx/sites-enabled/default
 service nginx restart
 
@@ -23,7 +23,7 @@ gunicorn --bind 0.0.0.0:8001 ownphotos.wsgi &
 
 
 
-sed -i -e 's/http:\/\/changeme/'"$BACKEND_URL"'/g' /code/ownphotos-frontend/src/api_client/apiClient.js
+sed -i -e 's/http:\/\/changeme/'"${BACKEND_PROTOCOL}:\/\/${BACKEND_HOST}"'/g' /code/ownphotos-frontend/src/api_client/apiClient.js
 cd /code/ownphotos-frontend
 npm run build
 serve -s build
